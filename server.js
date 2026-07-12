1aeb428ca638ee333bcd57f4de4aff9f4abab3d7
dkfzk <145877242+dkfzk@users.noreply.github.com>
2026-07-12 18:03:58 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+app.use(cors({
+    origin: [
+        "https://dkfzk.github.io",
+    ]
+}));
+
 const app = express();
 app.use(express.json());
 
